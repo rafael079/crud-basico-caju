ec13aa8f9f69c900a8be947d9322d0b935f8751d
Rafael de Oliveira Silva <ros.rock079@gmail.com>
2024-01-23 20:00:45 -0300

    Ajustes para Evitar SQL Injection

--- service/database.php
+++ service/database.php
@@ -14,20 +14,28 @@ class Database
         $this->connection = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->user, $this->password);
     }
 
-    public function query(string $sql)
+    public function query(string $sql, array $params)
     {
-        return $this->connection->query($sql);
+        $query = $this->connection->prepare($sql);
+        return $query->execute($params);
     }
 
     public function delete(int $id)
     {
-        return $this->connection->prepare("DELETE FROM post WHERE id={$id}");
+        return $this->query("DELETE FROM post WHERE id=:id", [':id' => $id]);
     }
 
     public function getAll()
     {
-        $query = $this->query("SELECT * FROM post ORDER BY id DESC");
+        $query = $this->connection->query("SELECT * FROM post ORDER BY id DESC");
 
         return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById(int $id)
+    {
+        $query = $this->connection->query("SELECT * FROM post WHERE id={$id}");
+
+        return $query->fetchObject();
     }
 }
